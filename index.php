c20d95a41dd20012290ee3e22800271efc7a352a
ModHussain <md.hussain.dg@gmail.com>
2018-08-12 17:23:07 +0530

    Signed-off-by: ModHussain <md.hussain.dg@gmail.com>

---
+++ index.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mahathi Techno Solutions</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="css/style.css" rel="stylesheet" type="text/css">
+   <link rel="stylesheet" href="css/animate.css">
+   <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+
+  <style>
+  
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fulid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage"><img src="images/cctvs.png" class="siz img-circle" width="100%"></a><b class="colo"><a href="#myPage" class="navbar-brand" >Mahathi Techno Soft</b></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-center">
+        <li><a href="#myPage"> <i class="fa fa-home icon"></i> HOME</a></li>
+        <li><a href="#band"><i class="fa fa-cogs icon"></i> FEATURES</a></li>
+        <li><a href="#about"><i class="fa fa-users icon"></i> ABOUT US</a></li>
+        <li><a href="#services"><i class="fa fa-handshake-o icon"></i> SERVICES</a></li>  
+        <li><a href="#contact"><i class="fa fa-phone-square icon"></i> CONTACT US</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br>
+<div id="myCarousel" class="carousel slide space top" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+   
+    </ol>
+
+    <div class="carousel-inner">
+
+
+    <div class="item active">
+      <img src="images/2.jpg" alt="2" style="width:100%;">
+      <div class="carousel-caption">
+      <h1>Installation and Maintenance of CCTV Cameras for Home</h1>
+      </div>
+     </div>
+    
+    <div class="item">
+      <img src="images/banner2.jpg" alt="3" style="width:100%;">
+      <div class="carousel-caption">
+       <h1 class="co"> Website Development and Mobile Apps development (Android and IOS)</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/banner4.jpg" alt="4" style="width:100%;">
+      <div class="carousel-caption">
+        <h1 class="">Solar Is Clean and Safe energy to utilize</h1>
+      </div>
+    </div>
+  
+    </div>
+    
+
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+<!-- Container (The Band-->
+<br id="band">
+<br>
+<div  class="container  text-center " >
+  <h2 class="text-center"><i class="fa fa-cogs"></i> OUR FEATURES</h2> 
+  <br>
+ <h3 class="text-center ">Mahathi CCTV Camera Range</h3> 
+    <p class="text-center">Stay in the know wherever you go with Eurovigil’s best in class CCTV cameras. Equipped with advanced technology it enables you to effortlessly monitor your property and the well being of your loved ones at all times.</p>
+    <h5 class="text-center"><b>Browse through our wide range of CCTV cameras that fulfill different requirements and pick the one that suits your need the best.</b></h5>
+    <p class="text-center"><strong>  Avaliable BRANDS:</strong></p>
+    <p class="text-center"><strong> <font color="blue"> CP pluses , Hik Vision,Zebronic ,Panasonic </font> </strong></p>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p class="text-center"><strong>CCTV – I VIEW HD200 (IR DOME)</strong></p>
+      <a href="#demo" data-toggle="collapse">
+        <img src="images/img2.jpg" class="person img-responsive" alt="Random Name" >
+
+      </a>
+      <div id="demo" class="collapse"> 
+        <p>1.3 Megapixel High-Performance CMOS</p>
+              <p>Turbo HD Output , up to 720P resolution</p>
+              <p>True Day/Night</p>
+              <p>Up to 20m IR distance</p>
+              <p>IP66 Weather proof</p>
+              <p>3.6 mm lens</p>
+              <p>6 mm lens</p>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <p class="text-center"><strong>CCTV – I VIEW HD300</strong></p>
+      <a href="#demo2" data-toggle="collapse">
+        <img src="images/img3.jpg" class="img-responsive person" alt="Random Name">
+      </a>
+      <div id="demo2" class="collapse">
+       <p>Turbo HD Output , up to 720P resolution</p>
+             <p>1.3 Megapixel High-Performance CMOS</p>
+             <p>True Day/Night, DNR, mdart IR</p>
+              <p>Up to 20m IR distance</p>
+              <p>IP66 Weather proof</p>
+              <p>3.6 mm lens</p>
+              <p>6 mm lens</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>CCTV – I VIEW HD400 </strong></p>
+      <a href="#demo3" data-toggle="collapse">
+        <img src="images/img4.png" class="img-responsive person" alt="Random Name" >
+      </a>
+      <div id="demo3" class="collapse">
+        <p>1/3" Progressive Scan CMOS</p>
+              <p>1280x720 / 1920x1080</p>
+              <p>23x Optical Zoom</p>
+              <p>DNR, mdart IR, DWDR</p>
+              <p>Upto 100m IR distance</p>
+              <p>3D intelligent positioning function</p>
+              <p>Support Turbo HD and BNC output</p>
+      </div>
+    </div>
+  </div>
+<!--second row-->
+<div class="row">
+    <div class="col-sm-4">
+      <p class="text-center"><strong>EUROVIGIL I VIEW HD 500 PTZ</strong></p>
+      <a href="#demo4" data-toggle="collapse">
+        <img src="images/img5.jpg" class="person img-responsive" alt="Random Name" >
+
+      </a>
+      <div id="demo4" class="collapse"> 
+       <p>1/3" Progressive Scan CMOS</p>
+              <p>1280x720 / 1920x1080</p>
+              <p>23x Optical Zoom</p>
+              <p>DNR, mdart IR, DWDR</p>
+              <p>Upto 100m IR distance</p>
+              <p>3D intelligent positioning function</p>
+              <p>Support Turbo HD and BNC output</p>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <p class="text-center"><strong>EUROVIGIL I VIEW HD 700 IP DOME</strong></p>
+      <a href="#demo5" data-toggle="collapse">
+        <img src="images/img6.jpg" class="img-responsive person" alt="Random Name">
+      </a>
+      <div id="demo5" class="collapse">
+       <p>Turbo HD Output , up to 720P resolution</p>
+             <p>1.3 Megapixel High-Performance CMOS</p>
+             <p>True Day/Night, DNR, mdart IR</p>
+              <p>Up to 20m IR distance</p>
+              <p>IP66 Weather proof</p>
+              <p>3.6 mm lens</p>
+              <p>6 mm lens</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>EUROVIGIL I VIEW HD 800 IP BULLET </strong></p>
+      <a href="#demo6" data-toggle="collapse">
+        <img src="images/img7.jpg" class="img-responsive person" alt="Random Name" >
+      </a>
+      <div id="demo6" class="collapse">
+        <p>1/3" Progressive Scan CMOS</p>
+              <p>1280x720 / 1920x1080</p>
+              <p>23x Optical Zoom</p>
+              <p>DNR, mdart IR, DWDR</p>
+              <p>Upto 100m IR distance</p>
+              <p>3D intelligent positioning function</p>
+              <p>Support Turbo HD and BNC output</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="bg-1">
+  <div class="container">
+    <h3 class="text-center">Website Developement & Solar Energy</h3>
+    <p class="text-center">We works on Website development,Mobile Apps development (Android and IOS) and E-Commerce Websites</p>
+    <h5 class="text-center"><p class="text-center">Solar Is Clean and Safe energy to utilize. Solar is a safe alternative which can replace current fossil fuels like coal and gas for generation of electricity that produce air, water, and land pollution. ... Use of solar energy will eliminate these unsafe, unclean consequences from using conventional fossil fuels.</p></h5>
+   
+    
+    <div class="row text-center">
+      <div class="col-sm-4">
+        <h5><b></b></h5>
+        <div class="thumbnail wow bounceInLeft">
+          <img src="images/ab1.gif" alt="Paris" width="400" height="300">
+          
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <h5><b></b></h5>
+        <div class="thumbnail wow bounceInUp">
+          <img src="images/ab2.gif" alt="New York" width="400" height="300">
+          
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <h5><b> </b></h5>
+        <div class="thumbnail wow bounceInRight">
+          <img src="images/ab3.gif" alt="San Francisco" width="400" height="300">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<br id="about">
+<div class="container" >
+  <h2  class="text-center"><i class="fa fa-users "></i> ABOUT US</h2> 
+  <br>
+    <h3 class="text-center">Mahathi Techno Soft </h3> 
+    <p class="text-center">Mahathi Techno Soft is Hyderabad based company. We provides solution for all kind of digital security as per your need we design installation and maintenance of CCTV cameras for home and commercial properties and we have presence in Telangana and Andhra Pradesh. We are leading provider of wide range of security cameras suitable for residential flats, villas, individual homes, commercial complexes, shops, gated communities, offices, warehouses and small businesses, including analog High Definition (HD, HDCVI, HDTVI) and IP cameras, fully installed at competitive prices. </p>
+<p>Mahathi Techno Soft we are aim to grow in the digital space we are coming with innovative solution in digital space nothing like. We provide Digital security, Software development, and solar solutions.
+</p>
+      <div class="row">
+        <div class="col-md-3"><div class="border1"><h5 class="text-center"> Display with <b>Video Door Phone</b></h5><img src="images/5.jpg" class="img-responsive"></div></div>
+        <div class="col-md-3"><div class="border1"><h5 class="text-center"> Deter with <b>Access Control System</b></h5><img src="images/6.jpg" class="img-responsive"></div></div>
+        <div class="col-md-3"><div class=" border1"><h5 class="text-center"> Detect with <b>Intruder Alarm System</b></h5><img src="images/7.jpg" class="img-responsive"></div></div>
+        <div class="col-md-3"><div class=" border1"><h5 class="text-center"> Discover with <b>CCTV Surveillance</b></h5><img src="images/8.jpg" class="img-responsive"></div></div>
+      </div>
+  </div>
+
+<br>
+<br id="services">
+<div  class="bg-1">
+  <div class="containers">
+    <h2  class="text-center"><i class="fa fa-handshake-o icon "></i>  SERVICES
+     </h2>
+  
+   <div class="row">
+    <div class="col-sm-4 wow bounceInLeft">
+      <p class="text-center"><strong>Digital Security</strong></p>
+      <a href="#demose" data-toggle="collapse">
+        <img src="images/service1.gif" class=" img-responsive" alt="Random Name" >
+
+      </a>
+      <div id="demose" class="collapse text-center"> 
+       <p>• CCTV cameras</p>
+        <p>•  Biometric systems</p>
+<p>•  Video conference systems</p>
+      </div>
+    </div>
+    
+    <div class="col-sm-4 wow bounceInUp">
+      <p class="text-center"><strong>Software Development </strong></p>
+      <a href="#demoser" data-toggle="collapse">
+        <img src="images/service2.gif" class="img-responsive" alt="Random Name">
+      </a>
+      <div id="demoser" class="collapse text-center">
+       <p>• Website development</p>
+        <p>•  Mobile Apps development (Android and IOS)</p>
+<p>•  E-commerce sites</p>
+      </div>
+    </div>
+    <div class="col-sm-4 wow bounceInRight" >
+      <p class="text-center"><strong>Solar </strong></p>
+      <a href="#demoserv" data-toggle="collapse">
+        <img src="images/service3.gif" class="img-responsive " alt="Random Name" >
+      </a>
+      <div id="demoserv" class="collapse text-center">
+        <p>•  Solar for Home</p>
+        <p>•  Solar for Agriculture</p>
+        <p>•  Solar for commercials</p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+</div>
+
+<!-- Container (Contact Section) -->
+<div id="contact" class="containers">
+  <h2 class="text-center"> <i class="fa fa-phone-square"></i> CONTACT US</h2>
+  <p class="text-center"><em>We love our customers</em></p>
+
+  <div class="row">
+    <div class="col-md-4">
+      <p>Our Address</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> &nbsp;Mahathi Techno Soft Pvt.Ltd.Plot.No:-4, Road. No: - 02,Karmanghat Hyderabad,Telangana-500079.</p>
+      <p><span class="glyphicon glyphicon-phone"></span>Phone: 7095858889</p>
+      <p><span class="glyphicon glyphicon-envelope"></span>Email: sales@mahathi.co.in</p>
+    </div>
+    <div class="col-md-8">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
+      <br>
+      <div class="row">
+        <div class="col-md-12 form-group">
+          <button class="btn pull-right" type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+ 
+
+  <div class="tab-content">
+    
+    
+    
+  </div>
+</div>
+
+<div id="googleMap"></div>
+<script>
+function myMap() {
+var myCenter = new google.maps.LatLng(17.342026, 78.534619);
+var mapProp = {center:myCenter, zoom:13, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5bh-Wqv3OCmSSHW3vC81FmnlNC0huEVA&callback=myMap"></script>
+
+
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p>Mahathi Techno Soft <a href="#myPage" data-toggle="tooltip" title="Visit CCTV SECURITY">www.mahathi.co.in</a></p> 
+</footer>
+
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+</script>
+
+</body>
+</html>
